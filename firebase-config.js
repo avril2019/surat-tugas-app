d557e8af0c49f23d9c13f2711ed917cecd7cfd3d
avril2019 <avrilaviw2019@gmail.com>
2025-06-26 09:41:54 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,9 +1,17 @@
-// Ganti dengan konfigurasi Firebase milikmu
-export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_ID",
-  appId: "YOUR_APP_ID"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDEIQm3lG9gTLcZrNZ9Q2M0Xo25X0E24kw",
+  authDomain: "surattugasapp.firebaseapp.com",
+  projectId: "surattugasapp",
+  storageBucket: "surattugasapp.firebasestorage.app",
+  messagingSenderId: "341828655101",
+  appId: "1:341828655101:web:1b8f3954cffbd204ffab94"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
